327226f91b75568942ef0c4fbdb81ba784c46769
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-07-04 14:03:39 +0700

    Update

--- QuanLyBanHang/Quanlybanhang.sql
+++ QuanLyBanHang/Quanlybanhang.sql
@@ -1,1 +1,26 @@
 create database quanlybanhang;
+use quanlybanhang;
+-- CREATE TABLE `customer` (
+--   `cID` int NOT NULL,
+--   `cName` varchar(45) DEFAULT NULL,
+--   `cAge` int DEFAULT NULL,
+--   PRIMARY KEY (`cID`));
+
+-- CREATE TABLE `order` (
+--   `oID` int NOT NULL,
+--   `cID` int DEFAULT NULL,
+--   `oDate` date DEFAULT NULL,
+--   `oTotalPrice` double DEFAULT NULL,
+--   PRIMARY KEY (`oID`));
+
+-- CREATE TABLE `orderdetail` (
+--   `oID` int NOT NULL,
+--   `pID` int DEFAULT NULL,
+--   `odQTY` int DEFAULT NULL,
+--   PRIMARY KEY (`oID`));
+
+-- CREATE TABLE `product` (
+--   `pID` int NOT NULL,
+--   `pName` varchar(45) DEFAULT NULL,
+--   `pPrice` double DEFAULT NULL,
+--   PRIMARY KEY (`pID`));
