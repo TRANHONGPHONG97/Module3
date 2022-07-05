82c149de8e6fab7aee355d273400a2a5c3d2e0b2
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-07-05 14:07:28 +0700

    Update

--- 2. Thiết kế và tạo CSDL/Quanlybanhang.sql
+++ 2. Thiết kế và tạo CSDL/Quanlybanhang.sql
@@ -1,26 +1,24 @@
 create database quanlybanhang;
 use quanlybanhang;
--- CREATE TABLE `customer` (
---   `cID` int NOT NULL,
---   `cName` varchar(45) DEFAULT NULL,
---   `cAge` int DEFAULT NULL,
---   PRIMARY KEY (`cID`));
+CREATE TABLE `customer` (
+  `cID` int NOT NULL,
+  `cName` varchar(45) DEFAULT NULL,
+  `cAge` int DEFAULT NULL,
+  PRIMARY KEY (`cID`));
 
--- CREATE TABLE `order` (
---   `oID` int NOT NULL,
---   `cID` int DEFAULT NULL,
---   `oDate` date DEFAULT NULL,
---   `oTotalPrice` double DEFAULT NULL,
---   PRIMARY KEY (`oID`));
+CREATE TABLE `orders` (
+  `oID` int NOT NULL,
+  `cID` int DEFAULT NULL,
+  `oDate` date DEFAULT NULL,
+  `oTotalPrice` double DEFAULT NULL);
 
--- CREATE TABLE `orderdetail` (
---   `oID` int NOT NULL,
---   `pID` int DEFAULT NULL,
---   `odQTY` int DEFAULT NULL,
---   PRIMARY KEY (`oID`));
+CREATE TABLE `orderdetail` (
+  `oID` int NOT NULL,
+  `pID` int DEFAULT NULL,
+  `odQTY` int DEFAULT NULL);
 
--- CREATE TABLE `product` (
---   `pID` int NOT NULL,
---   `pName` varchar(45) DEFAULT NULL,
---   `pPrice` double DEFAULT NULL,
---   PRIMARY KEY (`pID`));
+CREATE TABLE `product` (
+  `pID` int NOT NULL,
+  `pName` varchar(45) DEFAULT NULL,
+  `pPrice` double DEFAULT NULL)
+ 
--- 3. Thao tác với CSDL/ThaotacvoiCSDLQuanlybanhang.sql
+++ 3. Thao tác với CSDL/ThaotacvoiCSDLQuanlybanhang.sql
@@ -1,11 +1,11 @@
 use quanlybanhang;
 
 INSERT INTO Customer
-VALUES (1, 'Minh Quan', 10),
+VALUES  (1, 'Minh Quan', 10),
 		(2, 'Ngoc Oanh', 20),
-        (3, 'Hong Ha',50);
+        (3, 'Hong Ha', 50);
 
-INSERT INTO order(oID, cID, oDate)
+INSERT INTO orders(oID, cID, oDate)
 VALUES 	(1,1,'2006-3-21'),
 		(2,2,'2006-3-23'),
 		(3,1,'2006-3-16');
@@ -17,12 +17,37 @@ VALUES 	(1,'May Giat', 3),
         (4, 'Quat',1),
         (5, 'Bep Dien',2);
         
-INSERT INTO OrderDetails 
+INSERT INTO OrderDetail (oID, pID, odQTY)
 VALUES	(1,1,3),
 		(1,3,7),
         (1,4,2),
         (2,1,1),
         (3,1,8),
         (2,5,4),
-        (2,3,3);    
+        (2,3,3);
         
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+select oID, oDate, oTotalPrice
+from orders;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách (3 join 4 bảng)
+select orders.oID, customer.cID, cName, pName
+from orders  
+join customer on orders.cID =  customer.cID
+join orderdetail on orders.oID = orderdetail.oID
+join product on orderdetail.pID = product.pID;
+
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào (left join)
+select customer.cID, customer.cName
+from customer
+left join orders on orders.cID = customer.cID
+where orders.cID is null
+group by cName;
+
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
+-- (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. 
+-- Giá bán của từng loại được tính = odQTY*pPrice)
+select orders.oID, orders.oDate, orderdetail.odQTY * product.pPrice as oTotalPrice 
+from orders
+join orderdetail on orders.oID = orderdetail.oID
+join product on orderdetail.pID = product.pID;
