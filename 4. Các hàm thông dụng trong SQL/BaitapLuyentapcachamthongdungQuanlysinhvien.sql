ad7b715e041cb5bdd0371256a3a627827b883648
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-07-05 10:59:55 +0700

    update

--- 4. Các hàm thông dụng trong SQL/BaitapLuyentapcachamthongdungQuanlysinhvien.sql
+++ 4. Các hàm thông dụng trong SQL/BaitapLuyentapcachamthongdungQuanlysinhvien.sql
@@ -1,23 +1,21 @@
 use quanlysinhvien;
+
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-SELECT subid, subname, credit, avg(credit)
+select SubId, SubName, Credit, Status
 from subject
-group by subid
-having avg(credit) >=
-all (Select avg(credit) from `subject`
-group by `subject`.credit);
+group by SubId
+having avg(credit) >= all (select avg(credit) 
+from subject
+group by subject.credit);
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-select `subject`.subname, max(credit)
-from subject
-join mark on `subject`.subid = mark.subid
-group by credit;
-select * from `mark`;
-select * from `subject`;
+SELECT subject.subName, max(credit)
+FROM subject
+join Mark M on subject.subid = M.subid
+GROUP BY credit;
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-SELECT Student.*, AVG(Mark) as `Điểm trung bình`
-FROM student
-Join Mark ON Student.studentid = mark.studentid
-GROUP BY studentid
+SELECT S.StudentId, S.StudentName, AVG(Mark)
+FROM Student S join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
 order by AVG(mark) DESC;
